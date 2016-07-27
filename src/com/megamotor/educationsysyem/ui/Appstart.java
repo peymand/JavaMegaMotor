5e1c85d9552b6139603580f44700b06549feae22
3849 <3849@WKS-3849.mega.com>
2016-07-27 17:22:40 +0430

    my secound commit

--- src/com/megamotor/educationsysyem/ui/Appstart.java
+++ src/com/megamotor/educationsysyem/ui/Appstart.java
@@ -3,6 +3,6 @@ package com.megamotor.educationsysyem.ui;
 public class Appstart {
 	public static void main(String[] args) {
 
-		System.out.println("hiiii");
+		System.out.println("hiiiii");
 	}
 }
